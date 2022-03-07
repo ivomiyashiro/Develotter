e5e6aaff42632b231d7599e7cd22b127b1a40aee
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2022-03-07 09:25:30 -0300

    .

--- next.config.js
+++ next.config.js
@@ -3,20 +3,23 @@ module.exports = {
   reactStrictMode: true,
   env: {
     SECRET_JWT_SEED: process.env.SECRET_JWT_SEED,
-    NEXT_APP_API_URL: process.env.NEXT_APP_API_URL
+    NEXT_APP_API_URL: process.env.NEXT_APP_API_URL,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
   },
   async headers() {
-    return [{
-      source: '/auth/renew.ts',
-      headers: [
-        {
-          key: 'x-token',
-          value: ''
-        }
-      ]
-    }];
+    return [
+      {
+        source: '/auth/renew.ts',
+        headers: [
+          {
+            key: 'x-token',
+            value: '',
+          },
+        ],
+      },
+    ];
   },
   images: {
-    domains: ['res.cloudinary.com']
-  }
+    domains: ['res.cloudinary.com'],
+  },
 };
--- utils/database.ts
+++ utils/database.ts
@@ -5,7 +5,7 @@ let conn: any;
 if (!conn) {
   conn = new Pool ({
     user: 'ejcwirxzqqizsu',
-    password: 'df9545fc1e5dfa0215bfc070e235fe6f13092f694874754e290b8441d31aabda',
+    password: process.env.DATABASE_PASSWORD,
     host: 'ec2-3-227-195-74.compute-1.amazonaws.com',
     port: 5432,
     database: 'dd7d3pde4q3cvp'
